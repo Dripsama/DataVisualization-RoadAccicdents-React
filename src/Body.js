cd827befa00c8e0017dfb98b40d269626e9b4a61
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-05-21 19:07:40 +0530

    Sit5

--- src/Body.js
+++ src/Body.js
@@ -3,6 +3,7 @@ import Sit1 from "./Components/FirstSit/Sit1";
 import Sit2 from "./Components/SecondSit/Sit2";
 import Sit3 from "./Components/ThirdSit/Sit3";
 import Sit4 from "./Components/FourthSit/Sit4";
+import Sit5 from "./Components/FifthSit/Sit5";
 
 export default function Body() {
   return (
@@ -10,7 +11,8 @@ export default function Body() {
       {/* <Sit1 /> */}
       {/* <Sit2 /> */}
       {/* <Sit3 /> */}
-      <Sit4 />
+      {/* <Sit4 /> */}
+      <Sit5 />
     </div>
   );
 }
---
+++ src/Components/FifthSit/Sit5.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sit5.css";
+
+export default function Sit5() {
+  return (
+    <div>
+      <h1>This is Sit5</h1>
+    </div>
+  );
+}
